db87c89ba9634f896c50c055a6ab7205cfbd155b
Mike Vargas <vargonian@gmail.com>
2023-09-02 18:30:45 -0700

    Changing "Hello" response to use the display name of the user if it exists, otherwise the username.

--- goobo_bot.py
+++ goobo_bot.py
@@ -16,7 +16,8 @@ class MyClient(discord.Client):
         
         # Check if the message content matches "Hello Goobo"
         if re.match(r'Hello Goobo', message.content, re.IGNORECASE):
-            await message.channel.send(f'Hello, {message.author.name}!')
+            nickname = message.author.display_name
+            await message.channel.send(f'Hello, {nickname}!')
 
 intents = discord.Intents.default()
 intents.message_content = True
