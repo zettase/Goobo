02306bc35b6037fbcab8a9b8a5cd6d8cfa2fc980
zettase <141450845+zettase@users.noreply.github.com>
2023-09-14 18:17:01 -0700

    Added morning and night responses

--- goobo_bot.py
+++ goobo_bot.py
@@ -29,6 +29,14 @@ class MyClient(discord.Client):
         if re.match(r'Beeboobee', message.content, re.IGNORECASE):
             await message.channel.send(f'Beeboobee? That\'s what Turt is.')
 
+        # Check if message content matches ":kirby_sleep:" - Turt's goodnight response
+        if re.match(r':kirby_sleep:', message.content, re.IGNORECASE):
+            await message.channel.send(f'Buenas noches por favor.')
+
+        # Check if message content matches ":wave_crazy:" - Turt's morning response
+        if re.match(r':wave_crazy:', message.content, re.IGNORECASE):
+            await message.channel.send(f':sun_with_face: Hola muchacho~ :sun_with_face:')
+
     async def on_message_edit(self, before, after):
         database.handle_message_edit(before, after)
 
